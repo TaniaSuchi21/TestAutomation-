cdf4ab01d0132f883b76fb69800c82e2b5c3178a
TaniaSuchi21 <88900354+TaniaSuchi21@users.noreply.github.com>
2021-09-15 02:37:30 +0600

    TestNGBasic

---
+++ src/main/TestNGBasic.java
@@ -0,0 +1,23 @@
+package com.TestNG;
+
+import org.testng.annotations.Test;
+
+public class TestNGBasic {
+
+    @Test(enabled = false)
+    public static void testMethod1(){
+        System.out.println("TestNG");
+    }
+
+    @Test(priority = 1)
+    public static void testMethod2(){
+        System.out.println("Test Automation");
+    }
+    @Test(description = "Test Description")
+    public static void testMethod3(){
+        System.out.println("Selenium");
+    }
+
+
+
+}
